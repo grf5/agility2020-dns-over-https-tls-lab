7558b4d91c3b83e33d87b0ebac98eb4731e88bf7
g.robinson@f5.com <Seattle2016!>
2020-02-19 01:34:38 -0500

    fixing requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='agility2020-dns-over-https-tls-lab',
+    version='',
+    packages=[''],
+    url='https://github.com/grf5/agility2020-dns-over-https-tls-lab',
+    license='',
+    author='Greg Robinson',
+    author_email='g.robinson@f5.com',
+    description=''
+)
